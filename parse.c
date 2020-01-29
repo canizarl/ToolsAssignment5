80285ebf42a78727de8d67cd6bfb1240dd80a44f
canizarl <canizarl@tcd.ie>
2020-01-29 12:49:48 +0000

    K as default when no option passed

--- parse.c
+++ parse.c
@@ -16,7 +16,8 @@ void print_usage(void){
 
 
 void  parse_command_line(const int argc, char *const * argv,CLOptions *opts)
-{
+{   
+    int debugging = 1;
     /** 
      * This function takes an argument with an option and stores it in the system.
      * The user can also disply a usage statement to understand how to input numbers 
@@ -24,47 +25,89 @@ void  parse_command_line(const int argc, char *const * argv,CLOptions *opts)
     printf("\n");
     int option;
 
-
+    if(debugging == 1){
+        printf("\nargc: %d", argc);
+        printf("\nargv: %s", argv[1]);
+    }
     /*
     * 
     * 
     */
+
+    if(argc == 2){
+        printf("\n No option selected - DEFAULT: K \n");
+        opts -> number = atoll(argv[1]);
+        if(atoll(argv[1])==0){
+            fprintf(stderr, "\nERRROR Not a number or number not assigned correctly to opts ");
+        }
+        opts -> flag = 'K';
+
+    }
+
+
     while((option = getopt(argc,argv,"K:M:G:T:P:E:h:")) != -1 )
     {
+        
+        if(debugging == 1){
+            printf("\noption: %c", option);
+        }
+
         switch(option)
         {	
             case 'K':
                 opts -> number = atoll(optarg);
                 opts -> flag = 'K';
+                if ((atoll(optarg)) >=9223372036854775807){
+                    printf("\nWARNING INPUT NUMBER TOO BIG - MAX NUMBER is 9223372036854775807  9.0E \n");
+                }
+
                 break;
 
             case 'M':
                 opts -> number = atoll(optarg);
                 opts -> flag = 'M';
+                if ((atoll(optarg)) >=9223372036854775807){
+                    printf("\nWARNING INPUT NUMBER TOO BIG - MAX NUMBER is 9223372036854775807  9.0E \n");
+                }
                 break;            
             
             case 'G':
                 opts -> number = atoll(optarg);
                 opts -> flag = 'G';
+                if ((atoll(optarg)) >=9223372036854775807){
+                    printf("\nWARNING INPUT NUMBER TOO BIG - MAX NUMBER is 9223372036854775807  9.0E \n");
+                }
                 break;            
             
             case 'T':
                 opts -> number = atoll(optarg);
                 opts -> flag = 'T';
+                if ((atoll(optarg)) >=9223372036854775807){
+                    printf("\nWARNING INPUT NUMBER TOO BIG - MAX NUMBER is 9223372036854775807  9.0E \n");
+                }
                 break;            
             
             case 'P':
                 opts -> number =  atoll(optarg);
                 opts -> flag = 'P';
+                if ((atoll(optarg)) >=9223372036854775807){
+                    printf("\nWARNING INPUT NUMBER TOO BIG - MAX NUMBER is 9223372036854775807  9.0E \n");
+                }
                 break;            
             
             case 'E':
                 opts -> number = atoll(optarg);
                 opts -> flag = 'E';
+                if ((atoll(optarg)) >=9223372036854775807){
+                    printf("\nWARNING INPUT NUMBER TOO BIG - MAX NUMBER is 9223372036854775807  9.0E \n");
+                }
                 break; 
                 
             case 'h':
                 opts -> number = atoll(optarg);
+                if ((atoll(optarg)) >=9223372036854775807){
+                    printf("\nWARNING INPUT NUMBER TOO BIG - MAX NUMBER is 9223372036854775807  9.0E \n");
+                }
 
                 if (opts -> number < 1024){
                     opts -> flag = 'n';
@@ -90,19 +133,16 @@ void  parse_command_line(const int argc, char *const * argv,CLOptions *opts)
                 break;
             
             case '?':
-                if ((optopt == 'K') || (optopt == 'M') || (optopt == 'G') || (optopt == 'T') || (optopt == 'P')||(optopt == 'E')||(optopt == 'h')){
-                    printf("hello there");
-                    }
-
+                printf("\nUnknown option\n");
                 print_usage();
                 break;
             
             default:
-                print_usage();
-                fprintf(stderr, "getopt");
+                printf("DEFAULT!!!");
 
         }
     }
+
 
 
 
