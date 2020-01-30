b1e3ac3e0e64a7d5b96c8b350eb7be43d8f527d0
canizarl <canizarl@tcd.ie>
2020-01-30 15:49:08 +0000

    comments added to parseSeq.c

--- my_seq.c
+++ my_seq.c
@@ -13,14 +13,17 @@
 #include <stdlib.h>
 #include "parseSeq.h"
 #include <getopt.h>
+#include <math.h>
+#include <limits.h>
 
 void printresult(CLOptions *opts);
+int numd(int n);
 
 int main(int argc, char * const *argv)
 {  
     CLOptions opts;
     parse_command_line(argc,argv, &opts);
-
+    printresult(&opts);
 
 
 
@@ -30,5 +33,44 @@ int main(int argc, char * const *argv)
 }
 
 void printresult(CLOptions *opts){
-    
+    int const first = opts->first;
+    int const increment = opts -> increment;
+    int const last = opts -> last;
+
+    if(first > last){
+        fprintf(stderr, "ERROR: only increasing sequence supported\n");
+        exit(EXIT_FAILURE);
+    }
+    if(increment > ((last-first))){
+        fprintf(stderr, "ERROR: increment too large! \n");
+        exit(EXIT_FAILURE);
+    }
+
+    int i = first; 
+    int z;
+    int diff;
+    while(i<(last+1)){
+        if (opts->wflag){
+            diff = numd(last) - numd(i);
+            for(z = 0;z<diff; z++){
+                printf("0");
+            }
+            // printf("%d \n", i);
+        }
+        printf("%d \n", i);
+        // else {
+        //     printf(" %d \n",i);
+        // }
+        i = i+increment;
+    }
+
+
+}
+
+
+int numd (int i) {
+    if (i == 0){
+        return 1;
+    }
+    return floor (log10 (abs (i))) + 1;
 }
--- parseSeq.c
+++ parseSeq.c
@@ -1,7 +1,19 @@
 /**
  * \file parseSeq.c
  * \brief Parses command line options to my_seq.c
-        Check function print_usage for instructions
+        Adds integers to a struct containing data needed
+        for my_seq.c 
+
+        - if only one integer is given, it assumes it is the
+        last number in the sequence. 
+        - if two numbers are given, it assumes these are 
+        the first and the last number. 
+        - if three numbers are given then it assumes first, 
+        second is increment, and then third number is last. 
+        - If -w is passed then the program flags this function
+        and my_seq.c will add  '0' to equalize width padding. 
+        
+        Check function print_usage for instructions on how to use.
 
  * \author L. A. Canizares
  * \version 1.0
@@ -21,6 +33,7 @@
 void print_usage(void){
     /*
     * Default message display to tell the user how to use the parsing function
+    * first and increment are optional. If none is given default is 1
     */
     printf("\nusage: seq [-w] [first [incr]] last\n");
 }
@@ -28,11 +41,12 @@ void print_usage(void){
 
 void  parse_command_line(const int argc, char *const * argv,CLOptions *opts)
 {   
-    int debugging = 1; // change to 1 for extra information
+    int debugging = 0; // change to 1 for extra information
     int option;
     int i;
     
     if(debugging == 1){
+        /* display arguments */ 
         printf("\nargc: %d", argc);
         printf("\nargv: " );
         for(i = 1; i< argc; i++){
@@ -42,7 +56,17 @@ void  parse_command_line(const int argc, char *const * argv,CLOptions *opts)
         
     }
 
+    // Allocate memory for the arguments to be
+    // converted into numbers 
     int *seqnumbers = calloc(3,sizeof(int));
+    if (seqnumbers == NULL){
+	    perror("Error initialszing seqnumbers");
+	    exit(EXIT_FAILURE);
+    }
+
+
+    // check if the arguments given are indeed numbers
+    // and only take the number arguments
     int j = 0;
     for(i = 1; i<argc; i++){
         if(isnumber(argv[i])){
@@ -51,6 +75,8 @@ void  parse_command_line(const int argc, char *const * argv,CLOptions *opts)
         }   
     }
 
+    // store the number arguments to the *opts struct 
+    // if increment and first arent given default is 1
     if (j == 1){
         opts -> last = seqnumbers[0];
         opts -> increment = 1;
@@ -76,13 +102,12 @@ void  parse_command_line(const int argc, char *const * argv,CLOptions *opts)
     }
     
 
-
+    // handle the -w option
     while((option = getopt(argc,argv,"w")) != -1 )
     {
         switch(option)
         {	
             case 'w':
-                printf("CASE w\n");
                 opts->wflag = 1;
                 break;
 
@@ -99,30 +124,29 @@ void  parse_command_line(const int argc, char *const * argv,CLOptions *opts)
 
     }
 
-
+    // deallocate memory for seqnumbers 
     free(seqnumbers);
 
 }
 
+
+
 int isnumber(char const *stringp){
+    // Check if the string is made of unsigned int numbers 
+    // this means it does not support + or - prefixes
+    // i.e it assumes they are all positive
+
+
+    // first check that string isnt empty
     if(stringp == NULL || *stringp == '\0'){
         return 0;
     }
-
-    int dots = 0;
+    // this checks for numbers only. Anything else in
+    // the string will make this not an integer
     while(*stringp){
         char c = *stringp;
-        switch(c){
-            case '.':
-                if(++dots > 1){
-                    return 0;
-                }
-                break;
-
-            default:
-                if(c<'0' || c >'9'){
-                    return 0;
-                }
+        if(c<'0' || c >'9'){
+            return 0;
         }
         stringp++;
     }
